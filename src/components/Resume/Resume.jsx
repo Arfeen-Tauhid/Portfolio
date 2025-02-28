63cbc96828d40c1aac48f54a0f0f5a3749d1e640
Arfeen-Tauhid <arfeentauhid09@gmail.com>
2025-02-28 22:20:17 +0530

    updated Protfolio

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Resume.module.css'
-import pdf from "../../assets/Resume/Resume.png"
+import pdf from "../../assets/Resume/Resume.pdf"
 
 const Resume = () => {
   // const resumeUrl = "#toolbar=0&navpanes=0&scrollbar=0&height=480px&width=350px&margin-top=50px";
@@ -9,7 +9,14 @@ const Resume = () => {
     <section>
       <div className={styles.con}>
       <div className={styles.container}>
-        <iframe src={`${pdf}?#toolbar=0&navpanes=0&scrollbar=0&height=500px&width=30%`} frameborder='0' className={styles.Resume} style={{ border: "none"}} title="Resume Preview" ></iframe>
+      <object 
+    data={`${pdf}#toolbar=0&navpanes=0&scrollbar=0`} 
+    type="application/pdf" 
+    className={styles.Resume} 
+    style={{ width: "100%", height: "630px", border: "none" }} 
+    title="Resume Preview"
+>
+</object>
       </div>
       <div className={styles.downloadBtn}>
         <a href={pdf} target="_blank" rel="noopener noreferrer">Download Resume</a>
